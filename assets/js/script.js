77cea8354507e1a0195ac6db5d0d20fb249bc2c3
Priya Macpherson <priyamacpherson@Priyas-MacBook-Pro.local>
2020-12-17 15:25:00 -0800

    added formatting to drag and drop

--- assets/js/script.js
+++ assets/js/script.js
@@ -200,6 +200,7 @@ var dropZoneDragHandler = function(event) {
   var taskListEl = event.target.closest(".task-list");
   if (taskListEl) {
     event.preventDefault();
+    taskListEl.setAttribute("style", "background: rgba(68, 233, 255, 0.7); border-style: dashed;");
   }
 };
 
@@ -221,12 +222,22 @@ var dropTaskHandler = function(event) {
     statusSelectEl.selectedIndex = 2;
   }
 
+  dropZoneEl.removeAttribute("style");
+
   dropZoneEl.appendChild(draggableElement);
 };
+
+var dragLeaveHandler = function(event) {
+  var taskListEl = event.target.closest(".task-list");
+  if (taskListEl) {
+    taskListEl.removeAttribute("style");
+  }
+}
 
 pageContentEl.addEventListener("change", taskStatusChangeHandler);
 pageContentEl.addEventListener("click", taskButtonHandler);
 pageContentEl.addEventListener("dragstart", dragTaskHandler);
 pageContentEl.addEventListener("dragover", dropZoneDragHandler);
 pageContentEl.addEventListener("drop", dropTaskHandler);
+pageContentEl.addEventListener("dragleave", dragLeaveHandler);
 
